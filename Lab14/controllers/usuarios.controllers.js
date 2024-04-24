70bbc6edbf55580d25d9660513ce7ce3fe763ceb
MonicaMMartinezV <A01710965@tec.mx>
2024-04-23 23:23:29 -0600

    Upload Lab14

---
+++ Lab14/controllers/usuarios.controllers.js
@@ -0,0 +1,13 @@
+const model = require("../models/usuarios.model.js");
+
+module.exports.render_login = async(req, res) =>{
+    const usuarioLogueado = 
+        model.login("user","pass");
+    res.render("usuarios/login",{
+        user:usuarioLogueado
+    });
+}
+
+module.exports.do_login = async(req, res) =>{
+    res.status(200)
+}
