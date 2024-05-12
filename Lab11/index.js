9b5478ae57950e7ff7b5470786db6d0f76284616
MonicaMMartinezV <A01710965@tec.mx>
2024-05-12 17:38:28 -0600

    Upload changes Lab11

--- Lab11/index.js
+++ Lab11/index.js
@@ -1,14 +1,19 @@
 const http    = require('http');
 const express = require('express');
 const path    = require('path');
-const fs      = require('fs');
 const app     = express();
-const bodyParser = require('body-parser');
+const fs      = require('fs');
 
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
-const rutasFormulario = require('./routes/formulario.routes');
-app.use('/formulario', rutasFormulario);
+const rutasformulario = require('./routes/formulario.routes');
+app.use('/formulario', rutasformulario);
+
+const rutaslabNew = require('./routes/labNew.routes');
+app.use('/labNew', rutaslabNew);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Middleware
 app.use((request, response, next) => {
@@ -16,68 +21,16 @@ app.use((request, response, next) => {
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
+app.get('/', (request, response, next) => {
+    response.setHeader('Content-Type', 'text/plain');
+    response.send("URL index /888");
+    response.end(); 
+});
+
 app.use((request, response, next) => {
     console.log('Otro middleware!');
     response.status(404);
     response.send('¡Page Not Found!'); //Manda la respuesta
-});
-
-app.get('/', (request, response, next) => {
-    response.setHeader('Content-Type', 'text/plain');
-    response.send("URL index /");
-    response.end(); 
-});
-
-app.get('/test_json', (request, response, next) => {
-    response.setHeader('Content-Type', 'application/json');
-    response.json({code:200, msg:"Ok GET"});
-    response.end();  
-});
-
-app.post('/test_json', (request, response, next) => {
-    response.setHeader('Content-Type', 'application/json');
-    response.json({code:200, msg:"Ok POST"});
-    response.end();  
-});
-
-app.get('/test_html', (request, response, next) => {
-    response.setHeader('Content-Type', 'text/html');    
-    response.write(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="utf-8">
-            <title>Código en HTML</title>
-        </head>
-        <body>
-            <h1>hola mundo desde express</h1>
-        </body>
-        </html>
-    `);
-    response.end(); 
-});
-
-app.get('/form_method', (request, response, next) => {
-    response.setHeader('Content-Type', 'text/html');
-    const html = fs.readFileSync(path.resolve(__dirname, './form.html'), 'utf8')
-    response.write(html);
-    response.end();  
-});
-
-app.post('/form_method', (request, response, next) => {
-    const indice = Number(request.body.indice);
-    console.log(indice);
-    const imprimir = request.body.imprimir
-    console.log(imprimir);
-
-    for(var i = 1; i <= indice; i++){
-        console.log(imprimir)
-    }
-
-    response.setHeader('Content-Type', 'application/json');
-    response.statusCode = 200;
-    response.write('{code:200, msg:"Ok POST"}');
-    response.end(); 
 });
 
 const server = http.createServer( (request, response) => {    
--- Lab11/routes/labNew.routes.js
+++ Lab11/routes/labNew.routes.js
@@ -2,3 +2,72 @@ const express = require('express');
 const path    = require('path');
 const fs      = require('fs');
 const router = express.Router();
+
+router.get('/pregunta', (request, response, next) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.write(`
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Archivo JSON</title>
+    </head>
+    <body>
+        <h1>Archivo JSON</h1>
+        <p>Un archivo JSON es un formato de texto simple para el intercambio de datos. Es utilizado comúnmente para transmitir datos entre un servidor y un cliente en aplicaciones web. Está compuesto por pares de clave-valor y se asemeja mucho a la sintaxis de los objetos en JavaScript. Esto lo hace ideal para representar datos estructurados, como configuraciones, información de usuario, resultados de consultas de bases de datos, entre otros.</p>
+    </body>
+    </html>
+    `);
+    response.end();
+});
+
+router.get('/test_json', (request, response, next) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.json({code:200, msg:"Ok GET"});
+});
+
+router.post('/test_json', (request, response, next) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.json({code:200, msg:"Ok POST"}); 
+});
+
+router.get('/test_html', (request, response, next) => {
+    response.setHeader('Content-Type', 'text/html');    
+    response.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Código en HTML</title>
+        </head>
+        <body>
+            <h1>hola mundo desde express</h1>
+        </body>
+        </html>
+    `);
+    response.end(); 
+});
+
+router.get('/password', (request, response, next) => {
+    response.setHeader('Content-Type', 'text/html');
+    const html = fs.readFileSync(path.resolve(__dirname, './../passW.html'), 'utf8')
+    response.write(html);
+    response.end();  
+});
+
+router.post('/password', (request, response, next) => {
+    const password = request.body.password;
+    console.log(password);
+    const confirmPassword = request.body.confirmPassword
+    console.log(confirmPassword);
+});
+
+router.get('/tiendaEnLinea', (request, response, next) => {
+    response.setHeader('Content-Type', 'text/html');
+    const html = fs.readFileSync(path.resolve(__dirname, './../tiendaEnLinea.html'), 'utf8')
+    response.write(html);
+    response.end();  
+});
+
+module.exports = router;
